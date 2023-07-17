482d65d60db321f1f4deee8d5634fb403b324f6e
subramaniyan <subramaniyansrinivasan2003@gmail.com>
2023-07-17 13:54:13 +0530

    made some changes

--- app/history/page.jsx
+++ app/history/page.jsx
@@ -50,8 +50,8 @@ export default function History() {
                             return (
                                 <>
                                     <div className='shadow-md rounded-md space-y-2  flex flex-col p-2' key={dat}>
-                                        <div className='flex flex-row space-x-1 items-center'><div className='font-semibold'>{name}: </div><div className={`${dat.message.role!=='assistant'?`w-3/4`:``}bg-slate-500 p-2 rounded-md w-full`}>{dat.prompt}</div></div>
-                                        <div className='flex flex-row space-x-1 items-center'><div className='font-semibold'>Ai: </div><div className={`${dat.message.role!=='assistant'?`w-3/4`:``}bg-slate-500 p-2 rounded-md w-full`}>{dat.message.content}</div></div>
+                                        <div className='flex flex-row space-x-1 items-start'><div className='font-semibold md:mt-0 mt-2'>{name}: </div><div className={`${dat.message.role!=='assistant'?`w-3/4`:``}bg-slate-500 p-2 rounded-md w-full`}>{dat.prompt}</div></div>
+                                        <div className='flex flex-row space-x-1 items-start'><div className='font-semibold mt-2'>Ai: </div><div className={`${dat.message.role!=='assistant'?`w-3/4`:``}bg-slate-500 p-2 rounded-md w-full`}>{dat.message.content}</div></div>
                                         {/* <div className='p-2 px-2 bg-slate-500 rounded-md'><span className='font-semibold'>Ai: </span>{dat.message.content}</div> */}
                                     </div>
                                 </>
